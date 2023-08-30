96643997a12f48671a06e975c34aad1759f66b42
rohit789@ <princeshmr27@gmail.com>
2023-08-30 20:35:51 +0530

    InvoiceServiceTest added one method RULE 1

--- InvoiceServiceTest.java
+++ InvoiceServiceTest.java
@@ -6,11 +6,20 @@ import junit.framework.Assert;
 
 public class InvoiceServiceTest {
 	   @Test
-	   public void givenDistanceAndTimeShouldReturnTotalFare() {
-	   InvoiceGenerator invoiceGenerator =new InvoiceGenerator();
-	   double distance=2.0;
-	   int time =5;
-	  double fare= invoiceGenerator.calculateFare(distance,time);
-	  Assert.assertEquals(25, fare,1);
-	}
+	 public void givenDistanceAndTimeShouldReturnTotalFare() {
+	     InvoiceGenerator invoiceGenerator =new InvoiceGenerator();
+	     double distance=2.0;
+	     int time =5;
+	     double fare= invoiceGenerator.calculateFare(distance,time);
+	     Assert.assertEquals(25, fare,1);
+     }
+	  
+	  public void givenLessDistanceTimeReturnMinFare() {
+		    InvoiceGenerator invoiceGenerator =new InvoiceGenerator();
+	        double distance = 0.1;
+	        int time = 1;
+	        double fare = invoiceGenerator.calculateFare(distance,time);
+	        Assert.assertEquals(5, fare, 1);
+	    }
+	   
 }
